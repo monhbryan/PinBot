9fb08a3a427be4da0c6e6da9ea76c3a66c191414
ZomBry <monhbryan1@gmail.com>
2019-03-08 23:11:32 -0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 from discord.ext import commands
 import discord
 import asyncio
+import sqlitedict
 
 bot = commands.Bot(
     command_prefix="!",
@@ -20,34 +21,45 @@ async def on_ready():
     print('----------')
     await bot.change_presence(status=discord.Status.online, game=discord.Game(name="Active!"))
 
+@bot.command(description = "Changes the prefix of commands")
+async def prefix(PreFix):
+    bot.command_prefix=PreFix[0]
+    await bot.say("Prefix changed to: " + PreFix[0])
+
 @bot.command(description="What do you think?")
 async def ping():
     await bot.say("Pong")
 
-@bot.command(description="Adds two numbers together")
-async def add(left : int=0, right : int=0):
-    await bot.say(left + right)
-
-# GLOBAL VARIABLE TO HOLD PINNED MSGS - TO BE CHANGED    
-PinnedMsg = {}
+# DATABASE FOR PINS
+from sqlitedict import SqliteDict
+PinnedMsg = SqliteDict('./my_db.sqlite', autocommit=True)
 #
+
 @bot.command(description="Creates a pin")
-async def createPin(messageKey, message):
+async def createp(messageKey, message):
     PinnedMsg[str(messageKey)] = message
     await bot.say("Pinned message created successfully")
+    
+        
 @bot.command(description="Removes a pin")
-async def removePin(messageKey):
+async def removep(messageKey):
     del PinnedMsg[messageKey]
     await bot.say("Pinned message deleted successfully")
     
 @bot.command(description="Lists all pins")
-async def listPin():
-    await bot.say("Number of Pinned Items :"+str(len(PinnedMsg)))
-    await bot.say(str(PinnedMsg))
+async def listp():
+    await bot.say("Number of Pinned Items : " + str(len(PinnedMsg)))
+    for key, value in PinnedMsg.iteritems():
+        if len(value) < 100:
+            await bot.say(key + ' = ' + value)
+        else:
+            await bot.say(key + ' = ' + value[0:100] + '...')
 
 @bot.command(description="Shows a pinned message")
 async def pin(messageKey):
     await bot.say(str(PinnedMsg[messageKey]))
+
+
 
 #Bot token - Hidden for obv reasons
 from token1 import token
