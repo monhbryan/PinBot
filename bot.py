1d6de8137735a06fe17ad93008be4559c540f79e
ZomBry <monhbryan1@gmail.com>
2019-03-08 20:26:16 -0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -27,17 +27,19 @@ async def ping():
 @bot.command(description="Adds two numbers together")
 async def add(left : int=0, right : int=0):
     await bot.say(left + right)
-PinnedMsg = {}
 
+# GLOBAL VARIABLE TO HOLD PINNED MSGS - TO BE CHANGED    
+PinnedMsg = {}
+#
 @bot.command(description="Creates a pin")
 async def createPin(messageKey, message):
     PinnedMsg[str(messageKey)] = message
-
+    await bot.say("Pinned message created successfully")
 @bot.command(description="Removes a pin")
 async def removePin(messageKey):
     del PinnedMsg[messageKey]
     await bot.say("Pinned message deleted successfully")
-
+    
 @bot.command(description="Lists all pins")
 async def listPin():
     await bot.say("Number of Pinned Items :"+str(len(PinnedMsg)))
@@ -47,5 +49,6 @@ async def listPin():
 async def pin(messageKey):
     await bot.say(str(PinnedMsg[messageKey]))
 
+#Bot token - Hidden for obv reasons
 from token1 import token
 bot.run(token)
